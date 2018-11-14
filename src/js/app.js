acb61ed60127c5db657ed3e9ce137c74d513afa5
Asafwlo <wloasaf@gmail.com>
2018-11-14 10:29:57 +0200

    init model

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,80 @@
 import $ from 'jquery';
-import {parseCode} from './code-analyzer';
+import { parseCode } from './code-analyzer';
+
+var data;
+var functionDeclaration = ['id', 'params', 'body'];
+var blockStatement = ['body'];
+
+
+var functionDeclarationBody = ['BlockStatement'];
+var param = ['type', 'name'];
+var id = ['type', 'name'];
 
 $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
         let codeToParse = $('#codePlaceholder').val();
         let parsedCode = parseCode(codeToParse);
         $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
+        data = parsedCode;
+        createTable();
     });
 });
+
+function isBodyExist(object) {
+    for (var key in object) {
+        if (key == 'body')
+            return true;
+    }
+    return false;
+}
+
+function createRow(line, type, name, condition, value) {
+    var newRow = document.createElement('tr');
+    var lineCol = document.createElement('td');
+    var typeCol = document.createElement('td');
+    var nameCol = document.createElement('td');
+    var condCol = document.createElement('td');
+    var valCol = document.createElement('td');
+
+    lineCol.innerHTML = line;
+    typeCol.innerHTML = type;
+    nameCol.innerHTML = name;
+    condCol.innerHTML = condition;
+    valCol.innerHTML = value;
+
+    newRow.appendChild(lineCol);
+    newRow.appendChild(typeCol);
+    newRow.appendChild(nameCol);
+    newRow.appendChild(condCol);
+    newRow.appendChild(valCol);
+
+    return newRow;
+
+}
+
+function createTable() {
+    var row = 1;
+    var tableBody = document.getElementById('parsedTableBody');
+    var currentObj = data;
+   
+    do {
+        currentObj = currentObj.body;
+        var lineCol = row;
+        var typeCol = currentObj.type;
+        var nameCol = currentObj.id.name;
+        var condCol = '';
+        var valCol;
+    }
+    while (isBodyExist(currentObj));
+    for (var key in parsedCode) {
+        if (parsedCode.hasOwnProperty(key)) {
+            var newRow = document.createElement('tr');
+            var lineCol = document.createElement('td');
+            var typeCol = document.createElement('td');
+            var nameCol = document.createElement('td');
+            var condCol = document.createElement('td');
+            var valCol = document.createElement('td');
+        }
+    }
+}
+
---
+++ src/js/model.js
@@ -0,0 +1,187 @@
+class id {
+    constructor(type, name) {
+        this.type = type;
+        this.name = name;
+    }
+}
+
+class params {
+    constructor(objects) {
+        this.objects = objects;
+    }
+}
+
+class body {
+    constructor() {
+        if (new.target === body) {
+            throw new TypeError('Cannot construct Abstract instances directly');
+        }
+    }
+}
+
+class Identifier extends body{
+    constructor(type, name){
+        super();
+        this.type = type;
+        this.name = name;
+    }
+}
+
+class Literal extends body{
+    constructor(value, raw){
+        super();
+        this.value = value;
+        this.raw = raw;
+    }
+}
+
+class Expression {
+    constructor() {
+        if (new.target === Expression) {
+            throw new TypeError('Cannot construct Abstract instances directly');
+        }
+    }
+}
+
+class right {
+    constructor(type, value, raw) {
+        if (arguments.length === 3) {
+            this.type = type;
+            this.value = value;
+            this.raw = raw;
+        }
+        else {
+            this.type = new Expression(type);
+        }
+    }
+}
+
+class left {
+    constructor(type, name) {
+        this.type = type;
+        this.name = name;
+    }
+}
+
+class UnaryExpression extends Expression {
+    constructor(operator, argument, prefix) {
+        super();
+        this.operator = operator;
+        this.argument = new Argument(argument);
+        this.prefix = prefix;
+    }
+}
+
+
+class BinaryExpression extends Expression {
+    constructor(operator, left, right) {
+        super();
+        this.operator = operator;
+        this.left = new left(left.type, left.name);
+        this.right = new right(right.type, right.value, right.raw);
+    }
+}
+
+class UpdateExpression extends Expression {
+    constructor(operator, argument, prefix) {
+        super();
+        this.operator = operator;
+        this.argument = new Argument(argument);
+        this.prefix = prefix;
+    }
+}
+
+class AssignmentExpression extends Expression {
+    constructor(operator, left, right) {
+        super();
+        this.operator = operator;
+        this.left = new left(left.type, left.name);
+        this.right = new right(right.type, right.value, right.raw);
+    }
+}
+
+class Argument extends body{
+    constructor(argument){
+        super(argument);
+    }
+}
+
+class Test extends body{
+    constructor(test){
+        super(test);
+    }
+}
+
+class ReturnStatement{
+    constructor(argument){
+        this.argument = new Argument(argument);
+
+    }
+}
+
+class ForStatement extends body {
+    constructor(init, test, update, body) {
+        super();
+        this.init = new body(init);
+        this.test = new Test(test);
+        this.update = new Expression(update);
+        this.body = new body(body);
+    }
+}
+
+class WhileStatement extends body {
+    constructor(test, body) {
+        super();
+        this.test = new Test(test);
+        this.body = new body(body);
+    }
+}
+
+class ExpressionStatement extends body {
+    constructor(expression) {
+        super();
+        this.expression = new Expression();
+    }
+}
+
+class VariableDeclarator extends body {
+    constructor(id, init) {
+        super();
+        this.id = new id(id.type, id.name);
+        this.init = init;
+    }
+}
+
+class VariableDeclaration extends body {
+    constructor(declarations, kind) {
+        super();
+        this.declarations = declarations;
+        this.kind = kind;
+    }
+}
+
+class BlockStatementBody extends body {
+    constructor(listOfObjects) {
+        super();
+        this.listOfObjects = listOfObjects;
+    }
+}
+
+class BlockStatement extends body {
+    constructor(body) {
+        super();
+        this.body = new body(body);
+    }
+}
+
+class FunctionDeclaration extends body {
+    constructor(id, params, body, generator, expression, async) {
+        super();
+        this.id = new id(id.type, id.name);
+        this.params = new params(params);
+        this.body = new body(body);
+        this.generator = generator;
+        this.expression = expression;
+        this.async = async;
+    }
+}
